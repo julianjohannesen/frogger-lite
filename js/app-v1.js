8bd3fcd684ab5f2505456937a3b379fa3bd2f4ce
Julian Johannesen <julian.johannesen@gmail.com>
2018-08-13 21:01:17 -0400

    changed allowedKeys array to a constant

--- js/app-v1.js
+++ js/app-v1.js
@@ -1,33 +1,30 @@
 // Enemies our player must avoid
 const Enemy = function(){ this.sprite = '../images/enemy-bug.png'; };
-
 // Update the enemy's position
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter which will ensure the game runs at the same speed for all computers.
-};
+Enemy.prototype.update = function(dt) { /* Multiply any movement by the dt parameter to ensure the game runs at the same speed for all computers.*/ };
+// Draw the enemy on the screen
+Enemy.prototype.render = function() { ctx.drawImage(Resources.get(this.sprite), this.x, this.y); };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {ctx.drawImage(Resources.get(this.sprite), this.x, this.y);};
-
-// Now write your own player class
-const Player = function(){
-    this.sprite = '../images/char-boy.png';
+// Our player
+const Player = function(){ this.sprite = '../images/char-boy.png'; };
+// Update the player's position
+Player.prototype.update = function(){}
+// Draw the player on the screen
+Player.prototype.render = function(){ ctx.drawImage(Resources.get(this.sprite), this.x, this.y); }
+Player.prototype.handleInput = function(keyPress){
+    if(keyPress[0] === 37) moveRight;
+    if(keyPress[0] === )
 }
 
-Player.prototype.update = function(){}
-Player.prototype.render = function(){ ctx.drawImage(Resources.get(this.sprite), this.x, this.y); }
-Player.prototype.handleInput = function(allowedKeys){}
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
+// Array containing enemy instances
 const allEnemies = [new Enemy(), new Enemy(), new Enemy()];
 
-// Place the player object in a variable called player
+// Player instance
 const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {37: 'left', 38: 'up', 39: 'right', 40: 'down'};
+    const allowedKeys = {37: 'left', 38: 'up', 39: 'right', 40: 'down'};
     player.handleInput(allowedKeys[e.keyCode]);
 });
